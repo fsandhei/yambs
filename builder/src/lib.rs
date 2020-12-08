60f95894c8c6baeeab5bbf06301a3bbe6338b6ab
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2020-12-08 21:43:30 +0100

    Refactored tests to make use of make_mmk_file() to reduce amount of code lines for "arrange, act, assert" sections of testing.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -147,8 +147,8 @@ mod tests {
     use std::io::Write;
     use tempdir::TempDir;
 
-    fn make_mmk_file(dir_name: &str) -> (std::path::PathBuf, File, Mmk) {
-        let dir = TempDir::new(&dir_name).unwrap();
+    fn make_mmk_file(dir_name: &str) -> (TempDir, std::path::PathBuf, File, Mmk) {
+        let dir: TempDir = TempDir::new(&dir_name).unwrap();
         let test_file_path = dir.path().join("mymakeinfo.mmk");
         let mut file = File::create(&test_file_path)
                                 .expect("make_mmk_file(): Something went wrong writing to file.");
@@ -161,6 +161,7 @@ mod tests {
                       some_other_file.h \\
         
         \n").expect("make_mmk_file(): Something went wrong writing to file.");
+
         let mut mmk_data = Mmk::new();
         mmk_data.data.insert(String::from("MMK_SOURCES"), 
                              vec![String::from("some_file.cpp"), 
@@ -169,51 +170,28 @@ mod tests {
         mmk_data.data.insert(String::from("MMK_HEADERS"), 
                              vec![String::from("some_file.h"), 
                                   String::from("some_other_file.h")]);
+        
+        mmk_data.data.insert(String::from("MMK_DEPEND"), 
+                             vec![String::new()]);
+        mmk_data.data.insert(String::from("MMK_EXECUTABLE"), 
+                             vec![String::new()]);
 
-        (test_file_path, file, mmk_data)
+        (dir, test_file_path, file, mmk_data)
     }
     #[test]
     fn read_mmk_files_one_file() -> std::io::Result<()> {
         let mut builder = Builder::new();
-        let dir = TempDir::new("example")?;
-        let test_file = dir.path().join("mymakeinfo.mmk");
-        let mut file = File::create(&test_file)?;
+        let (_dir, test_file_path, mut file, mut expected) = make_mmk_file("example");
+        
         write!(
             file,
             "\
-        MMK_SOURCES = some_file.cpp \\
-                      some_other_file.cpp \\
-        \n
-        MMK_HEADERS = some_file.h \\
-                      some_other_file.h \\
-        
-        \n
-        
         MMK_EXECUTABLE = x"
         )?;
-        builder.read_mmk_files(&test_file).unwrap();
-        let mut expected = Mmk::new();
-
-        expected
-            .data
-            .insert(String::from("MMK_DEPEND"), vec![String::new()]);
+        builder.read_mmk_files(&test_file_path).unwrap();
         expected
             .data
             .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("some_file.cpp"),
-                String::from("some_other_file.cpp"),
-            ],
-        );
-        expected.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("some_file.h"),
-                String::from("some_other_file.h"),
-            ],
-        );
         assert_eq!(builder.mmk_data[0], expected);
         Ok(())
     }
@@ -221,49 +199,20 @@ mod tests {
     #[test]
     fn read_mmk_files_two_files() -> std::io::Result<()> {
         let mut builder = Builder::new();
-        let dir = TempDir::new("example")?;
-        let test_file = dir.path().join("mymakeinfo.mmk");
-
-        let dir_dep = TempDir::new("example_dep")?;
-        let test_file_dep = dir_dep.path().join("mymakeinfo.mmk");
-
-        let mut file = File::create(&test_file)?;
-        let mut file_dep = File::create(&test_file_dep)?;
+        let (_dir, test_file_path, mut file, mut expected_1)     = make_mmk_file("example");
+        let (dir_dep, test_file_dep_path, _file_dep, expected_2) = make_mmk_file("example_dep");
 
         write!(
             file,
             "\
-        MMK_DEPEND = {} \\
-        \n
-        MMK_SOURCES = some_file.cpp \\
-                      some_other_file.cpp \\
-        \n
-        MMK_HEADERS = some_file.h \\
-                      some_other_file.h \\
-        
+            MMK_DEPEND = {} \\
         \n
         
         MMK_EXECUTABLE = x",
             &dir_dep.path().to_str().unwrap().to_string()
         )?;
 
-        write!(
-            file_dep,
-            "\
-        MMK_SOURCES = /some/some_file.cpp \\
-                      /some/other_file.cpp \\
-        \n
-        MMK_HEADERS = /some/some_file.h \\
-                      /some/some_other_file.h \\
-        
-        \n
-        
-        MMK_EXECUTABLE = x"
-        )?;
-
-        builder.read_mmk_files(&test_file).unwrap();
-        let mut expected_1 = Mmk::new();
-        let mut expected_2 = Mmk::new();
+        builder.read_mmk_files(&test_file_path).unwrap();
 
         expected_1.data.insert(
             String::from("MMK_DEPEND"),
@@ -273,49 +222,15 @@ mod tests {
             .data
             .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
 
-        expected_1.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("some_file.cpp"),
-                String::from("some_other_file.cpp"),
-            ],
-        );
-        expected_1.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("some_file.h"),
-                String::from("some_other_file.h"),
-            ],
-        );
 
-        expected_2
-            .data
-            .insert(String::from("MMK_DEPEND"), vec![String::new()]);
-        expected_2
-            .data
-            .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected_2.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("/some/some_file.cpp"),
-                String::from("/some/other_file.cpp"),
-            ],
-        );
-        expected_2.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("/some/some_file.h"),
-                String::from("/some/some_other_file.h"),
-            ],
-        );
         assert_eq!(builder.mmk_data[1], expected_1);
         assert_eq!(builder.mmk_data[0], expected_2);
         assert_eq!(
             builder.mmk_dependencies[1],
             Dependency {
-                path: test_file,
+                path: test_file_path,
                 requires: vec![Box::new(Dependency {
-                    path: test_file_dep,
+                    path: test_file_dep_path,
                     requires: Vec::new()
                 })]
             }
@@ -326,70 +241,26 @@ mod tests {
     #[test]
     fn read_mmk_files_three_files_two_dependencies() -> std::io::Result<()> {
         let mut builder = Builder::new();
-        let dir = TempDir::new("example")?;
-        let test_file = dir.path().join("mymakeinfo.mmk");
-
-        let dir_dep = TempDir::new("example_dep")?;
-        let test_file_dep = dir_dep.path().join("mymakeinfo.mmk");
-
-        let second_dir_dep = TempDir::new("example_dep_second")?;
-        let second_test_file_dep = second_dir_dep.path().join("mymakeinfo.mmk");
-
-        let mut file = File::create(&test_file)?;
-        let mut file_dep = File::create(&test_file_dep)?;
-        let mut second_file_dep = File::create(&second_test_file_dep)?;
+        let (_dir, test_file_path, mut file, mut expected_1) 
+            = make_mmk_file("example");
+        let (dir_dep, test_file_dep_path, _file_dep, expected_2) 
+            = make_mmk_file("example_dep");
+        let (second_dir_dep, test_file_second_dep_path, _file_second_file_dep, expected_3) 
+            = make_mmk_file("example_dep");
 
         write!(
             file,
             "\
         MMK_DEPEND = {} \\
                      {} \\
-        \n
-        MMK_SOURCES = some_file.cpp \\
-                      some_other_file.cpp \\
-        \n
-        MMK_HEADERS = some_file.h \\
-                      some_other_file.h \\
         
         \n
-        
         MMK_EXECUTABLE = x",
             &dir_dep.path().to_str().unwrap().to_string(),
             &second_dir_dep.path().to_str().unwrap().to_string()
         )?;
 
-        write!(
-            file_dep,
-            "\
-        MMK_SOURCES = /some/some_file.cpp \\
-                      /some/other_file.cpp \\
-        \n
-        MMK_HEADERS = /some/some_file.h \\
-                      /some/some_other_file.h \\
-        
-        \n
-        
-        MMK_EXECUTABLE = x"
-        )?;
-
-        write!(
-            second_file_dep,
-            "\
-        MMK_SOURCES = /some/some_file.cpp \\
-                      /some/other_file.cpp \\
-        \n
-        MMK_HEADERS = /some/some_file.h \\
-                      /some/some_other_file.h \\
-        
-        \n
-        
-        MMK_EXECUTABLE = x"
-        )?;
-
-        builder.read_mmk_files(&test_file).unwrap();
-        let mut expected_1 = Mmk::new();
-        let mut expected_2 = Mmk::new();
-        let mut expected_3 = Mmk::new();
+        builder.read_mmk_files(&test_file_path).unwrap();
 
         expected_1.data.insert(
             String::from("MMK_DEPEND"),
@@ -400,76 +271,19 @@ mod tests {
             .data
             .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
 
-        expected_1.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("some_file.cpp"),
-                String::from("some_other_file.cpp"),
-            ],
-        );
-        expected_1.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("some_file.h"),
-                String::from("some_other_file.h"),
-            ],
-        );
-
-        expected_2
-            .data
-            .insert(String::from("MMK_DEPEND"), vec![String::new()]);
-        expected_2
-            .data
-            .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected_2.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("/some/some_file.cpp"),
-                String::from("/some/other_file.cpp"),
-            ],
-        );
-        expected_2.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("/some/some_file.h"),
-                String::from("/some/some_other_file.h"),
-            ],
-        );
-
-        expected_3
-            .data
-            .insert(String::from("MMK_DEPEND"), vec![String::new()]);
-        expected_3
-            .data
-            .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected_3.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("/some/some_file.cpp"),
-                String::from("/some/other_file.cpp"),
-            ],
-        );
-        expected_3.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("/some/some_file.h"),
-                String::from("/some/some_other_file.h"),
-            ],
-        );
-
         assert_eq!(builder.mmk_data[2], expected_1);
         assert_eq!(builder.mmk_data[1], expected_2);
         assert_eq!(builder.mmk_data[0], expected_3);
         assert_eq!(
             builder.mmk_dependencies[2],
             Dependency {
-                path: test_file,
+                path: test_file_path,
                 requires: vec![Box::new(Dependency {
-                    path: test_file_dep,
+                    path: test_file_dep_path,
                     requires: Vec::new()
                 }),
                 Box::new(Dependency {
-                    path: second_test_file_dep,
+                    path: test_file_second_dep_path,
                     requires: Vec::new()
                 })]
             }
@@ -480,71 +294,30 @@ mod tests {
     #[test]
     fn read_mmk_files_three_files_two_dependencies_serial() -> std::io::Result<()> {
         let mut builder = Builder::new();
-        let dir = TempDir::new("example")?;
-        let test_file = dir.path().join("mymakeinfo.mmk");
-
-        let dir_dep = TempDir::new("example_dep")?;
-        let test_file_dep = dir_dep.path().join("mymakeinfo.mmk");
-
-        let second_dir_dep = TempDir::new("example_dep_second")?;
-        let second_test_file_dep = second_dir_dep.path().join("mymakeinfo.mmk");
-
-        let mut file = File::create(&test_file)?;
-        let mut file_dep = File::create(&test_file_dep)?;
-        let mut second_file_dep = File::create(&second_test_file_dep)?;
+        let (_dir, test_file_path, mut file, mut expected_1) 
+        = make_mmk_file("example");
+    let (dir_dep, test_file_dep_path, mut file_dep, mut expected_2) 
+        = make_mmk_file("example_dep");
+    let (second_dir_dep, test_file_second_dep_path, _file_second_file_dep, expected_3) 
+        = make_mmk_file("example_dep");
 
         write!(
             file,
             "\
         MMK_DEPEND = {} \\
         \n
-        MMK_SOURCES = some_file.cpp \\
-                      some_other_file.cpp \\
-        \n
-        MMK_HEADERS = some_file.h \\
-                      some_other_file.h \\
-        
-        \n
-        
         MMK_EXECUTABLE = x",
-            &dir_dep.path().to_str().unwrap().to_string()            
-        )?;
+            &dir_dep.path().to_str().unwrap().to_string())?;
 
         write!(
             file_dep,
             "\
         MMK_DEPEND = {} \\
         \n
-        MMK_SOURCES = /some/some_file.cpp \\
-                      /some/other_file.cpp \\
-        \n
-        MMK_HEADERS = /some/some_file.h \\
-                      /some/some_other_file.h \\
-        
-        \n
-        
-        MMK_EXECUTABLE = x",
-        &second_dir_dep.path().to_str().unwrap().to_string()
-        )?;
+        ",
+        &second_dir_dep.path().to_str().unwrap().to_string())?;
 
-        write!(
-            second_file_dep,
-            "\
-        MMK_SOURCES = /some/some_file.cpp \\
-                      /some/other_file.cpp \\
-        \n
-        MMK_HEADERS = /some/some_file.h \\
-                      /some/some_other_file.h \\
-        
-        \n
-        
-        MMK_EXECUTABLE = x"
-        )?;
-
-        builder.read_mmk_files(&test_file).unwrap();
-        let mut expected_1 = Mmk::new();
-        let mut expected_2 = Mmk::new();
-        let mut expected_3 = Mmk::new();
+        builder.read_mmk_files(&test_file_path).unwrap();
 
         expected_1.data.insert(
             String::from("MMK_DEPEND"),
@@ -554,62 +327,9 @@ mod tests {
             .data
             .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
 
-        expected_1.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("some_file.cpp"),
-                String::from("some_other_file.cpp"),
-            ],
-        );
-        expected_1.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("some_file.h"),
-                String::from("some_other_file.h"),
-            ],
-        );
-
         expected_2
             .data
             .insert(String::from("MMK_DEPEND"), vec![second_dir_dep.path().to_str().unwrap().to_string()]);
-        expected_2
-            .data
-            .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected_2.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("/some/some_file.cpp"),
-                String::from("/some/other_file.cpp"),
-            ],
-        );
-        expected_2.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("/some/some_file.h"),
-                String::from("/some/some_other_file.h"),
-            ],
-        );
-
-        expected_3
-            .data
-            .insert(String::from("MMK_DEPEND"), vec![String::new()]);
-        expected_3
-            .data
-            .insert(String::from("MMK_EXECUTABLE"), vec![String::from("x")]);
-        expected_3.data.insert(
-            String::from("MMK_SOURCES"),
-            vec![
-                String::from("/some/some_file.cpp"),
-                String::from("/some/other_file.cpp"),
-            ],
-        );
-        expected_3.data.insert(
-            String::from("MMK_HEADERS"),
-            vec![
-                String::from("/some/some_file.h"),
-                String::from("/some/some_other_file.h"),
-            ],
-        );
 
         assert_eq!(builder.mmk_data[2], expected_1);
         assert_eq!(builder.mmk_data[1], expected_2);
@@ -617,12 +337,12 @@ mod tests {
         assert_eq!(
             builder.mmk_dependencies[2],
             Dependency {
-                path: test_file,
+                path: test_file_path,
                 requires: vec![Box::new(Dependency {
-                    path: test_file_dep,
+                    path: test_file_dep_path,
                     requires: vec![
                         Box::new(Dependency {
-                            path: second_test_file_dep,
+                            path: test_file_second_dep_path,
                             requires: vec![]
                         })]
                 })]

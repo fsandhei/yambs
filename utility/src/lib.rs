81dee241343ae0042a741c30547dfc407eee66b4
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2021-09-27 11:01:54 +0200

    Run formatter.

--- utility/src/lib.rs
+++ utility/src/lib.rs
@@ -1,106 +1,99 @@
 use error::{FsError, MyMakeError};
 use std::fs::File;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 
 // TODO: Vurder om denne skal returnere Result<PathBuf, MyMakeError>
 pub fn get_source_directory_from_path<P: AsRef<Path>>(path: P) -> PathBuf {
     if path.as_ref().join("source").is_dir() {
         return path.as_ref().join("source");
-    }
-    else if path.as_ref().join("src").is_dir() {
+    } else if path.as_ref().join("src").is_dir() {
         return path.as_ref().join("src");
-    }
-    else {
+    } else {
         return path.as_ref().to_path_buf();
     }
 }
 
-
 pub fn get_include_directory_from_path<P: AsRef<Path>>(path: P) -> Result<PathBuf, MyMakeError> {
     if path.as_ref().join("include").is_dir() {
         return Ok(path.as_ref().join("include"));
-    }
-    else {
+    } else {
         let parent = path.as_ref().parent().unwrap();
         if parent.join("include").is_dir() {
             return Ok(parent.join("include"));
+        } else {
+            return Err(MyMakeError::Generic {
+                description: format!("Could not find include directory from {:?}", parent),
+            });
         }
-        else {
-            return Err(MyMakeError::Generic {description: format!("Could not find include directory from {:?}", parent)});
-        }        
     }
 }
-
 
 pub fn get_mmk_library_file_from_path(path: &PathBuf) -> Result<PathBuf, MyMakeError> {
     if path.join("lib.mmk").is_file() {
         return Ok(path.join("lib.mmk"));
-    }
-    else {
-        return Err(MyMakeError::Generic {description: format!("{:?} does not contain a lib.mmk file!", path)});
+    } else {
+        return Err(MyMakeError::Generic {
+            description: format!("{:?} does not contain a lib.mmk file!", path),
+        });
     }
 }
-
 
 pub fn is_source_directory<P: AsRef<Path>>(path: P) -> bool {
-    (path.as_ref().ends_with("source") || path.as_ref().ends_with("src"))
-    && path.as_ref().is_dir()
+    (path.as_ref().ends_with("source") || path.as_ref().ends_with("src")) && path.as_ref().is_dir()
 }
-
 
 pub fn is_test_directory<P: AsRef<Path>>(path: P) -> bool {
     path.as_ref().ends_with("test")
 }
 
-
 pub fn get_head_directory(path: &Path) -> &Path {
     let part_to_strip = path.parent().unwrap();
-    return path.strip_prefix(part_to_strip).unwrap()
+    return path.strip_prefix(part_to_strip).unwrap();
 }
-
 
 // Add test for this function.
 pub fn get_project_top_directory(path: &Path) -> &Path {
     let parent = path.parent().unwrap();
     if is_source_directory(parent) || is_test_directory(parent) {
         return parent.parent().unwrap();
-    }
-    else {
+    } else {
         return parent;
     }
 }
-
 
 pub fn directory_exists(path: &Path) -> bool {
     path.exists()
 }
 
-
 pub fn create_dir<D: AsRef<Path>>(dir: D) -> Result<(), FsError> {
     if !dir.as_ref().is_dir() {
-        std::fs::create_dir_all(dir.as_ref()).map_err(|err| FsError::CreateDirectory(dir.as_ref().to_path_buf(), err))?;
+        std::fs::create_dir_all(dir.as_ref())
+            .map_err(|err| FsError::CreateDirectory(dir.as_ref().to_path_buf(), err))?;
     }
     Ok(())
 }
-
 
 pub fn remove_dir(dir: &std::path::PathBuf) -> Result<(), FsError> {
     if dir.is_dir() {
-        std::fs::remove_dir_all(dir).map_err(|err| FsError::RemoveDirectory(dir.to_path_buf(), err))?;
+        std::fs::remove_dir_all(dir)
+            .map_err(|err| FsError::RemoveDirectory(dir.to_path_buf(), err))?;
     }
     Ok(())
 }
 
-
-pub fn create_symlink<D, S>(destination: D, source: S) -> Result<(), FsError> 
-    where D: AsRef<Path>,
-          S: AsRef<Path> {
-       std::os::unix::fs::symlink(destination.as_ref(), source.as_ref())
-       .map_err(|err| FsError::CreateSymlink{ dest: destination.as_ref().to_path_buf(), 
-                                                 src: source.as_ref().to_path_buf(),
-                                                 source: err})
+pub fn create_symlink<D, S>(destination: D, source: S) -> Result<(), FsError>
+where
+    D: AsRef<Path>,
+    S: AsRef<Path>,
+{
+    std::os::unix::fs::symlink(destination.as_ref(), source.as_ref()).map_err(|err| {
+        FsError::CreateSymlink {
+            dest: destination.as_ref().to_path_buf(),
+            src: source.as_ref().to_path_buf(),
+            source: err,
+        }
+    })
 }
-
 
 pub fn create_file(dir: &PathBuf, filename: &str) -> Result<File, FsError> {
     let file = dir.join(filename);
@@ -118,13 +111,12 @@ pub fn print_full_path(os: &mut String, dir: &str, filename: &str, no_newline: b
     os.push_str(filename);
     if !no_newline {
         os.push_str(" \\\n");
-    }    
+    }
 }
 
-
-pub fn read_file(file_path: &Path) -> Result<String, FsError>
-{
-    std::fs::read_to_string(&file_path).map_err(|err| FsError::ReadFromFile(file_path.to_path_buf(), err))
+pub fn read_file(file_path: &Path) -> Result<String, FsError> {
+    std::fs::read_to_string(&file_path)
+        .map_err(|err| FsError::ReadFromFile(file_path.to_path_buf(), err))
 }
 
 #[cfg(test)]

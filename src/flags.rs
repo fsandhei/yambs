48d6c0939a8353a34f540455a3c505bde8e9ed8b
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2022-09-18 17:21:57 +0200

    Add traits and empty constructor for CompilerFlags.

--- src/flags.rs
+++ src/flags.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, serde::Deserialize, PartialEq, Eq)]
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
 pub struct CompilerFlags {
     #[serde(rename = "CXXFLAGS_APPEND")]
     cxx_flags: CXXFlags,
@@ -6,10 +6,19 @@ pub struct CompilerFlags {
     cpp_flags: CPPFlags,
 }
 
-#[derive(Debug, serde::Deserialize, PartialEq, Eq)]
+impl CompilerFlags {
+    pub fn new() -> Self {
+        Self {
+            cxx_flags: CXXFlags(Vec::new()),
+            cpp_flags: CPPFlags(Vec::new()),
+        }
+    }
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
 #[serde(transparent)]
 struct CXXFlags(std::vec::Vec<String>);
 
-#[derive(Debug, serde::Deserialize, PartialEq, Eq)]
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize, PartialEq, Eq)]
 #[serde(transparent)]
 struct CPPFlags(std::vec::Vec<String>);

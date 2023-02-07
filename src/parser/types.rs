f9961913ef644b624a398676d5bc6a9bb11a8c02
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2023-02-07 21:23:11 +0100

    default initialize if not present

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -56,9 +56,9 @@ pub struct RawCommonData {
     pub compiler_flags: CompilerFlags,
     #[serde(default)]
     pub defines: Vec<Define>,
-    #[serde(rename = "include_directories_append")]
+    #[serde(rename = "include_directories_append", default = "Vec::new")]
     pub include_directories: Vec<PathBuf>,
-    #[serde(rename = "system_include_directories_append")]
+    #[serde(rename = "system_include_directories_append", default = "Vec::new")]
     pub system_include_directories: Vec<PathBuf>,
 }
 
